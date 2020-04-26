9a8a823d925520f27f34dd0d5e7b3ffcd649f45e
EldirTorres <eldir.torres@gmail.com>
2020-04-26 01:21:21 -0400

    [Feature] add modified Application.Designer.vb

--- controlClient/My Project/Application.Designer.vb
+++ controlClient/My Project/Application.Designer.vb
@@ -32,7 +32,7 @@ Namespace My
 
         <Global.System.Diagnostics.DebuggerStepThroughAttribute()> _
         Protected Overrides Sub OnCreateMainForm()
-            Me.MainForm = Global.controlClient.DatosPersonales
+            Me.MainForm = Global.controlClient.Menu
         End Sub
     End Class
 End Namespace
