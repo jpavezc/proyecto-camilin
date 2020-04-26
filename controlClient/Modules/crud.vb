8b807cc7b68adf2ea66530fdb42f78cb7222970a
EldirTorres <eldir.torres@gmail.com>
2020-04-26 00:26:33 -0400

    [Feature] add structur

---
+++ controlClient/Modules/crud.vb
@@ -0,0 +1,21 @@
+﻿Imports MySql.Data.MySqlClient
+Module crud
+    Public Class conexion
+        Private MysqlCommand As New MySqlCommand
+        Dim MysqlConnString As String = "server=Miservidor; user id= myusuario ; password=myContraseña"
+        Public MysqlConexion As MySqlConnection = New MySqlConnection(MysqlConnString)
+
+        Public Sub ConectionMysql()
+            Try
+                MysqlConexion.Open()
+                MsgBox("la conexión fue exitosa")
+                MysqlConexion.Close()
+            Catch ex As Exception
+                MsgBox("La conexión no fue exitosa")
+            End Try
+        End Sub
+    End Class
+
+
+
+End Module
